834339669ed23dc95b7a4bb5cf7e00c27087ff19
Thiago <tg689092@gmail.com>
2023-09-02 22:07:57 -0300

    Constituindo o index e acrescentados os arquivos de header/footer para include

---
+++ models/header.php
@@ -0,0 +1,20 @@
+<?php
+
+echo '<header class="header">;
+<h1></h1>
+<nav class="header__nav">
+    <ul class="nav__menu">
+        <li class="nav__logo"><img src="" alt=""></li>
+        <li class="nav__item">Login</li>
+        <li class="nav__item">Cadastro</li>
+        <li class="nav__item">Cardápio</li>
+        <li class="nav__item">Meus Preferidos</li>
+        <li class="nav__item">Sobre</li>
+        <div class="nav__search">
+            <p><i class="fa-solid fa-magnifying-glass" style="color: #2c261c;"></i> Pesquisar</p>
+        </div>
+    </ul>
+</nav>
+</header>;';
+
+?>
