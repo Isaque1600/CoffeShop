13dd0c0338a7c59741dd6030f1ee660307e33f22
isaque1600 <isaquebatista25@gmail.com>
2023-09-18 21:30:02 -0300

    agora foi, eu acho

--- core/Config.php
+++ core/Config.php
@@ -7,7 +7,12 @@ abstract class Config
 
     public function __construct()
     {
-        define("DEFAULT_URL", "http://localhost/Projects-8.2/CoffeShop/");
+        $url = explode("/", __DIR__);
+        $urlDefault = "";
+        for ($i = 4; $i < count($url) - 1; $i++) {
+            $urlDefault .= $url[$i] . "/";
+        }
+        define("DEFAULT_URL", "http://localhost/" . $urlDefault);
 
         define("CSS_PATH", DEFAULT_URL . "assets/css/");
         define("JS_PATH", DEFAULT_URL . "assets/js/");
