45c7fa6da6d01d59bf9d234a2ed3a107abf11abf
isaque1600 <isaquebatista25@gmail.com>
2023-12-05 19:42:43 -0300

    imagens certas e produtos aprecendo normalmente

--- app/sts/Views/menu/menu.php
+++ app/sts/Views/menu/menu.php
@@ -34,18 +34,19 @@
             </div>
         </section>
         <section class="menu">
-            <div class="menu__item-container">
-                <?php
+            <?php
 
-                foreach($data['produtos'] as $key => $value) {
-                    echo "<div class=\"img-container\"><img src=\"".IMG_PATH.$value['path'].".png"."\" alt=\"\" class=\"item-img\"></div>";
-                    echo "<div class=\"item-details\">";
-                    echo "<p>".$value['nome']."</p>";
-                    echo "<b>R$".$value['valor']."</b>";
-                    echo "</div>";
-                }
-                ?>
-            </div>
+            foreach($data['produtos'] as $key => $value) {
+                echo "<div class=\"menu__item-container\">";
+                echo "<div class=\"img-container\"><img src=\"".IMG_PATH.$value['path'].".png"."\" alt=\"\" class=\"item-img\"></div>";
+                echo "<div class=\"item-details\">";
+                echo "<p>".$value['nome']."</p>";
+                echo "<b>R$".$value['valor']."</b>";
+                echo "</div>";
+                echo "</div>";
+            }
+
+            ?>
         </section>
     </main>
 </body>
