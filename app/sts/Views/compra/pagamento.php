0469d287c386a51bf4dcea3b541529ab832ff685
Thiago <tg689092@gmail.com>
2023-12-09 16:26:43 -0300

    Seu pedido é uma ordem, aqui estão as páginas de compra

--- app/sts/Views/compra/pagamento.php
+++ app/sts/Views/compra/pagamento.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo CSS_PATH ?>head-foot.css">
+    <link rel="stylesheet" href="<?php echo CSS_PATH ?>finalizar.css">
     <title>Pagamento</title>
 </head>
 
@@ -15,24 +17,35 @@
 
     ?>
 
-    <form action="" method="post">
-        <select name="formaPagamento" id="formaPagamento">
-            <option value="pix">PIX</option>
-            <option value="debito">Cartão de Debito</option>
-        </select>
-        <label for="cpf_cnpj" hidden>Cpf ou Cnpj</label>
-        <input type="text" name="cpf_cnpj" id="cpf_cnpj" hidden>
-        <label for="numeroCartao" hidden>Numero do Cartão</label>
-        <input type="text" name="numeroCartao" id="numeroCartao" hidden>
-        <label for="cvv" hidden>Codigo de segurança</label>
-        <input type="text" name="cvv" id="cvv" hidden>
-        <label for="dataVencimento" hidden>Data de Vencimento do cartão</label>
-        <input type="text" name="dataVencimento" id="dataVencimento" hidden>
-        <label for="nomeNoCartao" hidden>Nome no Cartão</label>
-        <input type="text" name="nomeNoCartao" id="nomeNoCartao" hidden>
-        <input type="submit" value="Confirmar" name="submit">
-    </form>
+    <main class="pay-form-wrapper">
+        <form action="" method="post" class="pay-form">
+            <select name="formaPagamento" id="formaPagamento">
+                <option value="pix">PIX</option>
+                <option value="debito">Cartão de Debito</option>
+            </select>
+            <div class="input-wrapper">
+                <label for="cpf_cnpj">Cpf ou Cnpj: </label>
+                <input type="text" name="cpf_cnpj" id="cpf_cnpj">
+            </div>
+            <div class="input-wrapper">
+                <label for="numeroCartao">Numero do Cartão: </label>
+                <input type="text" name="numeroCartao" id="numeroCartao">
+            </div>
+            <div class="input-wrapper">
+                <label for="cvv">Codigo de segurança: </label>
+                <input type="text" name="cvv" id="cvv">
+            </div>
+            <div class="input-wrapper">
+                <label for="dataVencimento">Data de Vencimento do cartão: </label>
+                <input type="text" name="dataVencimento" id="dataVencimento">
+            </div>
+            <div class="input-wrapper">
+                <label for="nomeNoCartao">Nome no Cartão: </label>
+                <input type="text" name="nomeNoCartao" id="nomeNoCartao">
+            </div>
+            <input type="submit" value="Confirmar" name="submit">
+        </form>
+    </main>
 
 </body>
-
 </html>
